524c467754a189f070375e78e026c8635582e90e
Paige Harris <hello@productpaige.com>
2026-01-13 20:38:05 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -124,7 +124,7 @@ export default function Home() {
       </div>
 
       {/* Divider */}
-      <div className="h-px bg-[#CACED3] my-12" />
+      <div className="h-px bg-[#CACED3] my-12 -mx-4 lg:-mx-8 w-screen relative left-1/2 -translate-x-1/2" />
 
       {/* Saved Prompts Section */}
       {savedPrompts.length > 0 && (
